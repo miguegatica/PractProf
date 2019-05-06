20b63965c7c41345b97c42b1df853a9b6e448671
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2019-05-06 17:28:17 -0300

    Commit sin zona de venta.

--- cliente/cliente_main.php
+++ cliente/cliente_main.php
@@ -19,8 +19,7 @@ Fny<?php
                     <th field="tipodocumento_id" width="50" hidden="true" sortable="true">Tipo documento</th>
                     <th field="tipodoc_descripcion" width="50" sortable="true" >Tipo documento</th> 
 
-                    <th field="zonaventa_id" width="50" hidden="true" sortable="true">Tipo zona</th> 
-                    <th field="zonaventa_descripcion" width="50" sortable="true" >Tipo zona</th>
+             
 
                 </tr>
             </thead>
@@ -68,10 +67,6 @@ Fny<?php
                 <!--        ***************** Combobox zonaventa *****************************************    -->
 
 
-                <div style="margin-bottom:10px">  
-                    <input name="zonaventa_id" id="zonaventa_id" style="width:100%">
-                </div>
-
 
             </form>
 
@@ -94,14 +89,13 @@ Fny<?php
 
             var url;
             var urlTiposDoc = 'cliente/utils.php?metodo=tiposdoclist';//desde el front estoy mandando los datos al backend por el metodo get
-            var urlZonasVentas = 'cliente/utils.php?metodo=zonasVentas';
+
 
 
 
 
             function nuevoCliente() {
                 $('#tipodocumento_id').combobox('reload', urlTiposDoc); //cada vez que agrega un nuevo cliente recarga los datos 
-                $('#zonaventa_id').combobox('reload', urlZonasVentas);
                 $('#dlgCliente').dialog('open').dialog('center').dialog('setTitle', 'Nuevo Cliente');
                 $('#fmCliente').form('clear');
                 url = 'cliente/cliente_save.php';
@@ -223,43 +217,6 @@ Fny<?php
 
 
 
-            $('#zonaventa_id').combobox({
-                url: urlZonasVentas,
-                valueField: 'id',
-                textField: 'text',
-                required: true,
-                label: 'Zona de venta:',
-                onChange: myKeyUpZona
-            });
-
-
-            function myKeyUpZona() {
-                var cc = $('#zonaventa_id');
-                var cantElementosVisibles = cc.combobox('panel').find('div').length;
-                var cantElementosOcultos = cc.combobox('panel').find('div:hidden').length;
-                var difVisiblesOcultos = Number(cantElementosVisibles) - Number(cantElementosOcultos);
-                console.log("cantElementosVisibles", cantElementosVisibles);
-                console.log("cantElementosOcultos", cantElementosOcultos);
-                console.log("difVisiblesOcultos", difVisiblesOcultos);
-                if (difVisiblesOcultos == 0) {
-                    $('#zonaventa_id').combobox('panel').find('div:hidden').css('display', 'block');
-                }
-
-            }
-
-
-            var zonaventa_id = $('#zonaventa_id').combobox('textbox');
-            zonaventa_id.bind('keydown', function (e) {
-                var cc = $('#zonaventa_id');
-                if (e.key == 'F1' || e.key == 'OS') {
-                    if (cc.combobox('panel').parent().css('display') === 'none') {
-                        cc.combobox('showPanel');
-                    } else {
-                        cc.combobox('hidePanel');
-                    }
-                }
-
-            });
 
 
 
