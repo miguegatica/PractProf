5ea62ab607c72b0c5f50a1ba9710697056c4a1e5
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2019-07-25 15:36:09 -0300

    PASO AL MASTER

---
+++ plandeCuenta/index_pdc.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+!<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" type="text/css" href="lib/bootstrap/css">
+    </head>
+    <body>
+
+    </body>
+</html>
+
