8b88860060dfd9c0b3d740ac78e8d7ee1d64513a
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2019-07-27 17:02:03 -0300

    Merge FormSupervisor
    Conflicts:
    	nbproject/private/private.xml

---
+++ componentes/tabla.php
@@ -0,0 +1,36 @@
+<?php
+?>
+
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+    <div class="container">
+               <div class="row">
+               <div class="col-sm-12">
+                   <h2>Plan de Cuenta</h2>
+                   <table class="table table-hover table-condensed table-bordered">
+                       <tr>
+                           <td>CÓDIGO PLAN</td>
+                           <td>NÙMERO</td>
+                           <td>NOMBRE</td>
+                           <td>TIPO</td>
+                           <td>EDITAR</td>
+                           <td>ELIMINAR</td>
+                       </tr>
+                        <tr>
+                           <td></td>
+                           <td></td>
+                           <td></td>
+                           <td></td>
+                       </tr>
+
+                   </table>
+
+               </div>
+           </div>
+   </div>
+    </body>
+</html>
+
--- plandeCuenta/index_pdc.php
+++ plandeCuenta/index_pdc.php
@@ -2,13 +2,27 @@
 
 ?>
 
-!<html>
+<html>
     <head>
-        <title>title</title>
-        <link rel="stylesheet" type="text/css" href="lib/bootstrap/css">
+        <title>Plan de cuenta</title>
+        <link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/alertify.css">
+        <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
+        
+        <script src="../librerias/jquery-1.9.1.min.js"></script>
+        <script src="../librerias/bootstrap/js/bootstrap.js"></script>
+        <script src="../librerias/alertifyjs/alertify.js"></script>
     </head>
     <body>
-
+        <div id="tabla"></div>
+        
+        <script type="text/javascript">
+        $(document).ready(function(){
+          $('#tabla').load('../componentes/tabla.php'); 
+            });
+        </script>
     </body>
+    
 </html>
+
 
