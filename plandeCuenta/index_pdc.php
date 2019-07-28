a7695a21930a8ea13377f8d624f361f35ecf50ca
Miguel Gatica <miguel_elias_gatica19@outlook.com>
2019-07-28 19:00:18 -0300

    commit y salgo

--- plandeCuenta/index_pdc.php
+++ plandeCuenta/index_pdc.php
@@ -4,16 +4,20 @@
 
 <html>
     <head>
-        <title>Plan de xuenta</title>
+        <title>Plan de cuenta</title>
         <link rel="stylesheet" type="text/css" href="../librerias/bootstrap/css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/alertify.css">
         <link rel="stylesheet" type="text/css" href="../librerias/alertifyjs/css/themes/default.css">
+     
         
         <script src="../librerias/jquery-1.9.1.min.js"></script>
         <script src="../librerias/bootstrap/js/bootstrap.js"></script>
         <script src="../librerias/alertifyjs/alertify.js"></script>
+      
     </head>
     <body>
+        <h1>hhh</h1>
+    
         <div id="tabla"></div>
         
         <script type="text/javascript">
@@ -21,6 +25,7 @@
           $('#tabla').load('../componentes/tabla.php'); 
             });
         </script>
+
     </body>
     
 </html>
